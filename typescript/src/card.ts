fc27b5d1088c93f880312a74cb042227694cfdd8
Katarina Murphy <katarigm@stud.ntnu.no>
2023-02-10 16:38:38 +1100

    blackjack finished

--- typescript/src/card.ts
+++ typescript/src/card.ts
@@ -1,8 +1,36 @@
 import { Suit } from "./suit";
 export class Card {
+  //if rank is 1, value will be 11. If not, normal rank
+  private value: number = this.rank === 1 ? 11 : this.rank;
   constructor(public readonly rank: number, private readonly suit: Suit) {}
 
   public get Suit() {
     return this.suit;
+  }
+
+
+  // 2. Returns cards with rank (11)Jack, (12)Queen, (13) King and (1)Ace as J, Q, K, A
+  public get CardLetter() {
+    if(this.rank === 13) {
+      return "K" 
+    }
+    if(this.rank === 12) {
+      return "Q" 
+    }
+    if(this.rank === 11) {
+      return "J" 
+    }
+    if(this.rank === 1) {
+      return "A" 
+    }
+    return this.rank
+  }
+
+  public get Value() {
+    return this.value;
+  }
+
+  public set Value(newValue: number) {
+    this.value = newValue;
   }
 }
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -7,18 +7,85 @@ const readConsole = readline.createInterface({
   terminal: true,
 });
 
+const blackJack = (deck: Deck, player: Array<Card | undefined>) => {
+  var total = player.reduce((total, card) => total + (card?.Value || 0), 0);
+  while(total > 21){
+    //finds ace with value of 11
+    const ace = player.find( card => card?.CardLetter === 'A' && card?.Value === 11)
+    //if no ace, break
+    if(!ace){
+      break;
+    } 
+    //sets ace value to 1
+    ace.Value = 1
+    //calculates updated total with new ace values
+    total = player.reduce((total, card) => total + (card?.Value || 0), 0);
+  }  
+
+  return total
+
+}
+
 async function main(whenFinished: () => void) {
   const deck = new Deck();
   const hand = new Array<Card | undefined>();
+  const dealer  = new Array<Card | undefined>();
 
   let playing = true;
+  //4. Cards put in randomized order
+  deck.cards.sort(function () { return 0.5 - Math.random();})
+  //Dealer is given a card at beginning of round
+  var dealerCard = deck.cards.pop();
+  dealer.push(dealerCard);
+
+  console.log(`Dealer's card is ${dealerCard?.Suit} ${dealerCard?.CardLetter}`)
+  //var count = 0;
   while (playing) {
     var card = deck.cards.pop();
-    hand.push(card);
-    var total = hand.reduce((total, card) => total + (card?.rank || 0), 0);
-    console.log(`Hit with ${card?.Suit} ${card?.rank}. Total is ${total}`);
+
+  //TEST for ace edge case (ex. A, A, 10 ==> 12 in stead of 22(1+11+10))
+   /*  var card = deck.cards.find( card => card?.CardLetter === 'A')
+    if(count > 1){
+      card = deck.cards.find( card => card?.rank === 10)
+    }
+    count = count + 1;
+    deck.cards = deck.cards.filter(item => item !== card)
+    */
+
+    hand.push(card); 
+    var total = blackJack(deck, hand)
+    console.log(`Player hit with ${card?.Suit} ${card?.CardLetter}. Total is ${total}`);
+    //1. If total over 21, break 
+    if (total>21){
+      console.log("Player lost")
+      break;
+    }
+    //5. dealer plays
     await readConsole.questionAsync("Stand, Hit (s/h) \n").then((read) => {
       if (read !== "h") {
+        let dealerPlaying = true;
+        //Dealder draws a card until they have 17 points or more
+        while(dealerPlaying){
+          dealerCard = deck.cards.pop();
+          dealer.push(dealerCard);
+          var dealerTotal = blackJack(deck, dealer)
+          console.log(`Dealer hit with ${dealerCard?.Suit} ${dealerCard?.CardLetter}. Total is ${dealerTotal}`);
+          if (dealerTotal > 16){
+            break;
+          }
+        }
+        //6. Game announces which party won
+        const difference = dealerTotal-total;
+        if(dealerTotal>21 || difference < 0){
+          console.log("Player won");
+        }
+        else if (difference === 0) {
+          console.log("It's a tie")
+        }
+        else{
+          console.log("Dealer won");
+        }
+
         playing = false;
       }
     });
